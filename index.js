cec8b41ff85669d5667e7b4a4a77b3f356502ac4
QuantumObstruction <chewje@oregonstate.edu>
2020-07-16 15:20:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const request = require('request');
 const argv = require('yargs').argv;
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 let apiKey = '843397ebf59e59ab18f5e5733c88938e';
 let city = argv.c || 'portland';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+
+
 
 request(url, function (err, response, body) {
   if(err){
