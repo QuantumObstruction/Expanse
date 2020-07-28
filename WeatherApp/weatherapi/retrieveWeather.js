471131707b55c3b01772d1ade5e2c4379a5229fa
matyevaa <59642740+matyevaa@users.noreply.github.com>
2020-07-28 14:47:48 -0700

    Update retrieveWeather.js

--- WeatherApp/weatherapi/retrieveWeather.js
+++ WeatherApp/weatherapi/retrieveWeather.js
@@ -40,34 +40,32 @@ function retrieveZipCodeWeather(req,res,context,idx,callback) {
       idx += 1;
       if(!err && response.statusCode < 400){
         let weather = JSON.parse(body);
-        let message0 = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        let message1 = `In ${weather.name} the forecast is currently ${weather.weather[0].description}`;
-        let message2 = `It feel's like ${weather.main.feels_like} degrees in ${weather.name}!`;
-        let message3 = `${weather.name} has projected High's of ${weather.main.temp_max} degrees`;
-        let message4 = `${weather.name} has projected Low's of ${weather.main.temp_min} degrees`;
-        let message5 = `${weather.name} Atmospheric Pressure ${weather.main.pressure} hPa`;
-        let message6 = `${weather.name} Humidity ${weather.main.humidity} %`;
-        let message7 = `${weather.name} Visibilty ${(weather.visibility)} ft`;
-        let message8 = `${weather.name} Wind Speed: ${(weather.wind.speed)} mph`;
-        let message9 = `${weather.name} Wind Speed: ${(weather.wind.speed)} mph`;
-        
-        console.log(message0);
-        console.log(message1);
-        console.log(message2);
-        console.log(message3);
-        console.log(message4);
-        console.log(message5);
-        console.log(message6);
-        console.log(((weather.visibility)/5280).toPrecision(2) + " miles");
-        console.log(message8);
-        console.log(message9);
+          let message00 = `The forecast for ${weather.name}:`;
+          let message0 = `It is ${weather.main.temp} degrees in ${weather.name}`;
+          let message1 = `The forecast is currently ${weather.weather[0].description}`;
+          let message2 = `It feels like ${weather.main.feels_like} degrees`;
+          let message3 = `The highest temp is ${weather.main.temp_max} degrees`;
+          let message4 = `The lowest temp is ${weather.main.temp_min} degrees`;
+          let message5 = `Atmospheric Pressure ${weather.main.pressure} hPa`;
+          let message6 = `Humidity ${weather.main.humidity}%`;
+          let message7 = `Visibilty ${(weather.visibility)} ft`;
+          let message8 = `Wind Speed: ${(weather.wind.speed)} mph`;
+          let message9 = `Wind Speed: ${(weather.wind.speed)} mph.`;
+          
+          console.log(message0);
+          console.log(message1);
+          console.log(message2);
+          console.log(message3);
+          console.log(message4);
+          console.log(message5);
+          console.log(message6);
+          console.log(((weather.visibility)/5280).toPrecision(2) + " miles");
+          console.log(message8);
+          console.log(message9);
 
-        curr_weather = 'The current temperature in ' +
-                   `${weather.name}` + 
-                   ' is ' +
-                   `${weather.main.temp}` +
-                   ' degrees.';
-        context.current.push({"weather":curr_weather});
+          curr_weather = message00 + '\n' + message0 + '\n' + message1 + '\n' + message2  + '\n' + message3 + '\n' + message4 + '\n' + message5 + '\n' + message6 + '\n' + message7 + '\n' + message8 + '\n' + message9;
+          context.current.push({"weather":curr_weather});
+        
       } else {
         console.log(err);
         console.log(response.statusCode);
@@ -115,16 +113,17 @@ function retrieveCityWeather(req,res,context,idx,callback) {
         idx += 1;
         if(!err && response.statusCode < 400){
           let weather = JSON.parse(body);
-          let message0 = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-          let message1 = `In ${weather.name} the forecast is currently ${weather.weather[0].description}`;
-          let message2 = `It feel's like ${weather.main.feels_like} degrees in ${weather.name}!`;
-          let message3 = `${weather.name} has projected High's of ${weather.main.temp_max} degrees`;
-          let message4 = `${weather.name} has projected Low's of ${weather.main.temp_min} degrees`;
-          let message5 = `${weather.name} Atmospheric Pressure ${weather.main.pressure} hPa`;
-          let message6 = `${weather.name} Humidity ${weather.main.humidity} %`;
-          let message7 = `${weather.name} Visibilty ${(weather.visibility)} ft`;
-          let message8 = `${weather.name} Wind Speed: ${(weather.wind.speed)} mph`;
-          let message9 = `${weather.name} Wind Speed: ${(weather.wind.speed)} mph`;
+          let message00 = `The forecast for ${weather.name}:`;
+          let message0 = `It is ${weather.main.temp} degrees in ${weather.name}`;
+          let message1 = `The forecast is currently ${weather.weather[0].description}`;
+          let message2 = `It feels like ${weather.main.feels_like} degrees`;
+          let message3 = `The highest temp is ${weather.main.temp_max} degrees`;
+          let message4 = `The lowest temp is ${weather.main.temp_min} degrees`;
+          let message5 = `Atmospheric Pressure ${weather.main.pressure} hPa`;
+          let message6 = `Humidity ${weather.main.humidity}%`;
+          let message7 = `Visibilty ${(weather.visibility)} ft`;
+          let message8 = `Wind Speed: ${(weather.wind.speed)} mph`;
+          let message9 = `Wind Speed: ${(weather.wind.speed)} mph.`;
           
           console.log(message0);
           console.log(message1);
@@ -137,12 +136,9 @@ function retrieveCityWeather(req,res,context,idx,callback) {
           console.log(message8);
           console.log(message9);
 
-          curr_weather = 'The current temperature in ' +
-                     `${weather.name}` + 
-                     ' is ' +
-                     `${weather.main.temp}` +
-                     ' degrees.';
+          curr_weather = message00 + '\n' + message0 + '\n' + message1 + '\n' + message2  + '\n' + message3 + '\n' + message4 + '\n' + message5 + '\n' + message6 + '\n' + message7 + '\n' + message8 + '\n' + message9;
           context.current.push({"weather":curr_weather});
+          
         } else {
           console.log(err);
           console.log(response.statusCode);
